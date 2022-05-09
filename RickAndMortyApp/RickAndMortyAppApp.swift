147602b015846a46df8360f34acdddef7b2bc4b7
Oğuzhan KERTMEN <iletisim@oguzhankertmen.com.tr>
2022-05-09 12:46:26 +0300

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Oğuzhan KERTMEN on 9.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
