80fb431e92ee2e272ad421f6bfd6062d89afa9dc
Oğuzhan KERTMEN <iletisim@oguzhankertmen.com.tr>
2022-05-17 17:19:23 +0300

    Apply SwiftLint

--- RickAndMortyApp/ViewModel/RickAndMortyViewModel.swift
+++ RickAndMortyApp/ViewModel/RickAndMortyViewModel.swift
@@ -8,38 +8,30 @@
 import Foundation
 import Apollo
 
-
-
 class CharacterViewModel: ObservableObject {
-    
+
     @Published var characters: [CharacterModel] = []
-    
-    func fetchCharacters(){
-        
+
+    func fetchCharacters() {
+
         Network.shared.apollo.fetch(query: FetchAllCharactersQuery()) { [weak self] result in
             switch result {
             case .success(let graphQLResult):
-                if let character  = graphQLResult.data?.characters?.results {
-                    self?.characters = character.map{CharacterModel(result: $0)}
-                    
+                if let character = graphQLResult.data?.characters?.results {
+                    self?.characters = character.map {CharacterModel(result: $0)}
+
                 }
             case .failure(let error):
                 print("Error: \(error)")
-                
+
             }
         }
-        
-       
+
     }
-    
+
     init() {
-        
+
         fetchCharacters()
     }
-    
+
 }
-
-
-
-
-
