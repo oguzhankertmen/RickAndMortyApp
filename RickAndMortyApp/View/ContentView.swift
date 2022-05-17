a89dc0bd7e82fef41538dee7dca388a1621f22c2
Oğuzhan KERTMEN <iletisim@oguzhankertmen.com.tr>
2022-05-17 17:17:28 +0300

    Apply SwiftLint

--- RickAndMortyApp/View/ContentView.swift
+++ RickAndMortyApp/View/ContentView.swift
@@ -9,55 +9,46 @@ import SwiftUI
 import KingfisherSwiftUI
 
 struct ContentView: View {
-    
+
     @StateObject var viewModel = CharacterViewModel()
-    
+
     var body: some View {
-        
-        
-        NavigationView{
-            
-            
-            List{
-            
+
+        NavigationView {
+
+            List {
+                
                 ForEach(viewModel.characters, id: \.name) { character in
-                
-                HStack {
-                    if let image = character.image,
-                       let url = URL(string: image) {
-                        KFImage(url)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 50, height: 50)
-                            .cornerRadius(25)
-                    } else {
-                        RoundedRectangle(cornerRadius: 25)
-                            .frame(width: 50, height: 50)
-                            .foregroundColor(.gray)
-                    }
-                    VStack(alignment: .leading) {
-                        Text(character.name )
-                            .font(.title3)
-                            .foregroundColor(.accentColor)
-                            .redacted(reason: character.name == nil ? .placeholder : [])
+                    HStack {
+                        if let image = character.image,
+                           let url = URL(string: image) {
+                            KFImage(url)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(25)
+                        } else {
+                            RoundedRectangle(cornerRadius: 25)
+                                .frame(width: 50, height: 50)
+                                .foregroundColor(.gray)
+                        }
+                        VStack(alignment: .leading) {
+                            Text(character.name )
+                                .font(.title3)
+                                .foregroundColor(.black)
+                                .redacted(reason: character.name == nil ? .placeholder : [])
 
-                    
+                        }
+
                     }
 
                 }
-                
-                }
-                
-            }
+
+            }.listStyle(DefaultListStyle())
+            .navigationBarTitle("Characters")
+
         }
-                    
-                
-            
-        
-        
-        
-        
-        
+
     }
 }
 
