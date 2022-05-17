c4d471eb2e6085c3c3ce848bfd09aba971fce1ae
Oğuzhan KERTMEN <iletisim@oguzhankertmen.com.tr>
2022-05-17 17:15:06 +0300

    Apply SwiftLint rules to the file

--- RickAndMortyApp/Model/CharacterModel.swift
+++ RickAndMortyApp/Model/CharacterModel.swift
@@ -7,19 +7,19 @@
 
 import Foundation
 
-
 struct CharacterModel {
-   
+
     var name: String
     var species: String
     var gender: String
     var image: String
-    
+
     init(result: FetchAllCharactersQuery.Data.Character.Result?) {
-        
+
         self.name = result?.name ?? "-"
         self.species = result?.species ?? "-"
         self.gender = result?.gender ?? "-"
         self.image = result?.image ?? "-"
+
     }
 }
