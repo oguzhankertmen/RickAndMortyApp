b012b14b2d66b90679868d4f5f6e24bfa93773aa
Oğuzhan KERTMEN <iletisim@oguzhankertmen.com.tr>
2022-05-23 16:53:34 +0300

    Add TabbarView

---
+++ RickAndMortyApp/View/TabbarView.swift
@@ -0,0 +1,31 @@
+//
+//  TabbarView.swift
+//  RickAndMortyApp
+//
+//  Created by Oğuzhan KERTMEN on 23.05.2022.
+//
+
+import SwiftUI
+
+struct TabbarView: View {
+    var body: some View {
+        TabView {
+            CharactersList()
+                .tabItem{
+                    Label("Characters", systemImage: "person.3")
+                }
+            SearchView()
+                .tabItem{
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+        }
+        .accentColor(.green)
+    }
+    
+}
+
+struct TabbarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabbarView()
+    }
+}
