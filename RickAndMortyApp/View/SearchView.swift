7c156c9424dd25458d0eae7e7725a52615a0886b
Oğuzhan KERTMEN <iletisim@oguzhankertmen.com.tr>
2022-05-23 16:53:59 +0300

    Create SearchView

---
+++ RickAndMortyApp/View/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  RickAndMortyApp
+//
+//  Created by Oğuzhan KERTMEN on 23.05.2022.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
